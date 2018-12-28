2a99e79c77bdc5ede79de3489e5d49cbbd7a2bb3
Jonathan Go <jogo88@gmail.com>
2018-12-28 15:21:33 +0900

    Initial Commit

---
+++ TinderFirestore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderFirestore
+//
+//  Created by Jonathan Go on 2018/12/28.
+//  Copyright © 2018 Appdelight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
